c27de74ffcac268841227375b9d0b6bf55f74dec
Josh <jhgann@gmail.com>
2022-12-22 14:37:28 +0100

    Changed return message

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello my dude',
+                message: 'Now using canary deployments!',
                 // location: ret.data.trim()
             })
         }
